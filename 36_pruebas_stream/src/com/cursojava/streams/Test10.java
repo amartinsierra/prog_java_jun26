b46cc68515c726f9a39e076a736b4e97f83a5c36
amartinsierra <ajms66@gmail.com>
2026-06-25 12:04:46 +0200

    proyectos 1 al 37

--- 36_pruebas_stream/src/com/cursojava/streams/Test10.java
+++ 36_pruebas_stream/src/com/cursojava/streams/Test10.java
@@ -1,10 +1,26 @@
 package com.cursojava.streams;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import java.util.stream.Stream;
+
 public class Test10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Stream<Integer> st=Stream.of(11,9,-2,3,8,11,-5,3);
+		//media de números positivos
+		/*System.out.println(
+			st.filter(n->n>0)
+			.mapToInt(n->n) //IntStream
+			.average() //OptionalDouble
+			.getAsDouble()
+		);*/
+		System.out.println(
+				st.filter(n->n>0)
+				.collect(Collectors.averagingDouble(n->n))
+			);
+		
 	}
 
 }
--- 36_pruebas_stream/src/com/cursojava/streams/Test9.java
+++ 36_pruebas_stream/src/com/cursojava/streams/Test9.java
@@ -1,9 +1,19 @@
 package com.cursojava.streams;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Test9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Stream<Integer> st=Stream.of(11,9,-2,3,8,11,-5,3);
+		//lista con los positivos
+		
+		List<Integer> positivos=st.filter(n->n>0)
+								//.collect(Collectors.toList())
+								.toList();
+								
 
 	}
 
